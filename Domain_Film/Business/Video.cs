22acfc5baadbe25aec1c5e50835b5e67f22a3b8a
AxelMyGuy <mail.axel.declercq@gmail.com>
2026-03-06 13:32:36 +0100

    Business met een coole klasse video

---
+++ Domain_Film/Business/Video.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		private int _id;
+	private string _name;
+
+	public int Id { get { return _id; } set { _id = value; } }
+    public string Name { get { return _name; } set { _name = value; } }
+}
+}
